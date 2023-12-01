55d444288edf144c8fa24029aed973e635138964
KevinAlvero <152252460+KevinAlvero@users.noreply.github.com>
2023-12-01 23:25:03 +0700

    Respository.java

--- src/Respository/Respository.java
+++ src/Respository/Respository.java
@@ -1,9 +1,13 @@
 package Respository;
 
-public interface Respository {
-	
-	public void find();
-	public void findOne();
-	public void insert();
-	
+import utility.Connection;
+
+import java.util.ArrayList;
+
+public interface Respository<T> {
+    ArrayList<T> find(String columnName, String[] conditions, boolean joinTable, String joinTableName, Connection connection);
+
+    T findOne(String columnName, String[] conditions, boolean joinTable, String joinTableName, Connection connection);
+
+    T insert(String[] data, Connection connection);
 }
