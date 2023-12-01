d89867e867f5fcb57d49f89a9b0d5fd3c5bbc1d4
KevinAlvero <152252460+KevinAlvero@users.noreply.github.com>
2023-12-01 23:25:17 +0700

    TeamRespository.java

--- src/Respository/TeamRespository.java
+++ src/Respository/TeamRespository.java
@@ -1,5 +1,38 @@
 package Respository;
 
-public class TeamRespository implements Respository {
+import utility.Connection;
 
+import java.util.ArrayList;
+
+import model.Team;
+
+public class TeamRespository implements Respository<Team> {
+    // ... (constructors, fields, etc.)
+
+    public TeamRespository(String string) {
+    }
+
+    @Override
+    public ArrayList<Team> find(String columnName, String[] conditions, boolean joinTable, String joinTableName, Connection connection) {
+        // Implement the find method based on the specified conditions
+        // ...
+        return new ArrayList<>();
+    }
+
+    @Override
+    public Team findOne(String columnName, String[] conditions, boolean joinTable, String joinTableName, Connection connection) {
+        // Implement the findOne method based on the specified conditions
+        // ...
+        return new Team(joinTableName, joinTableName);
+    }
+
+    @Override
+    public Team insert(String[] data, Connection connection) {
+        // Implement the insert method to insert data into the team.csv file
+        // ...
+        return new Team(null, null);
+    }
+
+    public void show(String columnName, String operator, String keyword) {
+    }
 }
