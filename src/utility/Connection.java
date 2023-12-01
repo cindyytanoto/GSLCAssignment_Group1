9207bd43d8aaeeaac205db6f149041e5df0418f7
KevinAlvero <152252460+KevinAlvero@users.noreply.github.com>
2023-12-01 23:24:44 +0700

    Connection.java

--- src/utility/Connection.java
+++ src/utility/Connection.java
@@ -1,5 +1,34 @@
 package utility;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Connection {
+	
+	private static Connection instance;
+	
+    private String filePath;
+	
+	private Connection(String filePath) {
+        this.filePath = filePath;
+	}
+	
+	public static Connection getInstance(String filePath) {
+		if(instance == null) {
+			instance = new Connection(filePath);
+		}
+		return instance;
+	}
+
+    public BufferedReader openFile() throws IOException {
+        return new BufferedReader(new FileReader(filePath));
+    }
+	
+    public FileWriter writeFile() throws IOException {
+        return new FileWriter(filePath, true);
+    }
+	
 
 }
