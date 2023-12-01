6b0abb0ef01c380ffc1a0105caf6f2d0acc56788
KevinAlvero <152252460+KevinAlvero@users.noreply.github.com>
2023-12-01 23:24:12 +0700

    Team.java

--- src/model/Team.java
+++ src/model/Team.java
@@ -1,6 +1,12 @@
 package model;
 
 public class Team {
-	
+    public String idTeam;
+    public String teamName;
+
+    public Team(String id, String teamName) {
+        this.idTeam = idTeam;
+        this.teamName = teamName;
+    }
 	
 }
