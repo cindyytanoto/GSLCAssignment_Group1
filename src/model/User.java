5a94be0997c3b1bedab0522f4c60806298af5937
KevinAlvero <152252460+KevinAlvero@users.noreply.github.com>
2023-12-01 23:24:24 +0700

    User.java

--- src/model/User.java
+++ src/model/User.java
@@ -1,5 +1,15 @@
 package model;
 
 public class User {
+    private String userName;
+    private String nim;
+    private String idTeam;
+    
 
+    public User(String userName, String nim, String idTeam) {
+        this.idTeam = idTeam;
+        this.userName = userName;
+        this.nim = nim;
+        
+    }
 }
