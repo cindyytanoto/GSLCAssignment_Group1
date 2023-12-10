48a879efc4393844c88c5d37858c4ac9964eb534
cindyytanoto <125437845+cindyytanoto@users.noreply.github.com>
2023-12-10 12:56:28 +0700

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,16 +1,13 @@
 package main;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 import model.Team;
 import model.User;
-import respository.TeamRespository;
-import respository.UserRespository;
+import repository.TeamRepository;
+import repository.UserRepository;
 import utility.Connection;
 
 public class Main {
@@ -18,32 +15,13 @@ public class Main {
 
 	private int idx;
 	private Scanner sc = new Scanner(System.in);
-	private String filePathTeams = "src/teams.csv";
-	private String filePathUser = "src/user.csv";
-	private UserRespository userRespository = new UserRespository();
-	private TeamRespository teamRespository = new TeamRespository();
+	private UserRepository userRepository = new UserRepository();
+	private TeamRepository teamRepository = new TeamRepository();
 
 	public Main() throws IOException {
-//		Connection conn = Connection.getInstance(filePathTeams);
-//		BufferedReader reader = conn.openFile();
-		// conn.closeFile(reader);
-
-//		idx = getTeamCount(filePathTeams);
 		mainMenu();
 		sc.close();
 	}
-
-//	private int getTeamCount(String filePath) {
-//		int count = 0;
-//		try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-//			while (reader.readLine() != null) {
-//				count++;
-//			}
-//		} catch (IOException e) {
-//			System.err.println("Error counting teams: " + e.getMessage());
-//		}
-//		return count;
-//	}
 
 	public void writeTeam() {
 		String teamName;
@@ -54,7 +32,7 @@ public class Main {
 	        ArrayList<Team> teamList = conn.readTeam();
 	        int idTeam = teamList.size() + 1; 
 
-	        teamRespository.insert(new Team(idTeam, teamName), conn);
+	        teamRepository.insert(new Team(idTeam, teamName), conn);
 	        System.out.println("Team Created!");
 	    } else {
 	        System.out.println("Error: Team name already exists. Please choose a unique team name.");
@@ -79,7 +57,7 @@ public class Main {
 		} else if (isTeamFull(idTeam)) {
 			System.out.println("Error: Team is full.");
 		} else {
-			userRespository.insert(new User(name, nim, idTeam), conn);
+			userRepository.insert(new User(name, nim, idTeam), conn);
 			System.out.println("User Created! (ID: " + idx + ")");
 		}
 	}
@@ -147,55 +125,45 @@ public class Main {
 	}
 
 	public void showMenu() {
-		System.out.println("Which table to show? 1. User, 2. Team.");
-		int tableChoice = sc.nextInt();
-		sc.nextLine();
+	    System.out.println("Which table to show? 1. User, 2. Team.");
+	    int tableChoice = sc.nextInt();
+	    sc.nextLine();
 
-		System.out.println("Want to filter by condition? 1. Yes, 2. No.");
-		int filterChoice = sc.nextInt();
-		sc.nextLine();
+	    String columnName = null;
+	    String operator = null;
+	    String keyword = null;
 
-		String columnName = null;
-		String operator = null;
-		String keyword = null;
+	    System.out.println("Want to filter by condition? 1. Yes, 2. No.");
+	    int filterChoice = sc.nextInt();
+	    sc.nextLine();
 
-		if (filterChoice == 1) {
-			System.out.println("Add condition, separate by semicolon.");
-			String condition = sc.nextLine();
+	    if (filterChoice == 1) {
+	        System.out.println("Enter filter condition (columnName operator keyword), separated by space:");
+	        String filterCondition = sc.nextLine();
+	        String[] conditionParts = filterCondition.split(" ");
 
-			// Split the condition by semicolon
-			String[] conditionParts = condition.split(";");
-			if (conditionParts.length == 3) {
-				columnName = conditionParts[0].trim();
-				operator = conditionParts[1].trim();
-				keyword = conditionParts[2].trim();
-			} else {
-				System.out.println("Invalid condition format. Please enter a valid condition.");
-				return;
-			}
-		}
+	        if (conditionParts.length == 3) {
+	            columnName = conditionParts[0].trim();
+	            operator = conditionParts[1].trim();
+	            keyword = conditionParts[2].trim();
+	        } else {
+	            System.out.println("Invalid condition format. Please enter a valid condition.");
+	            return;
+	        }
+	    }
 
-		// Now you have columnName, operator, and keyword.
-		// You can use these in your repository method call.
-		// For example:
-		// userRepository.find(columnName, new String[]{operator, keyword}, false, null,
-		// conn);
-
-		switch (tableChoice) {
-		case 1:
-			// Show users
-			System.out.println("Users:");
-//            userRespository.show(columnName, operator, keyword);
-			break;
-		case 2:
-			// Show teams
-			System.out.println("Teams:");
-//            teamRespository.show(columnName, operator, keyword);
-			break;
-		default:
-			System.out.println("Invalid table choice. Please enter 1 or 2.");
-			break;
-		}
+	    switch (tableChoice) {
+	        case 1:
+	            userRepository.show(columnName, operator, keyword, conn);
+	            break;
+	        case 2:
+//	        	System.out.println("dah masok");
+	            teamRepository.show(columnName, operator, keyword, conn);
+	            break;
+	        default:
+	            System.out.println("Invalid table choice. Please enter 1 or 2.");
+	            break;
+	    }
 	}
 
 	public void mainMenu() {
